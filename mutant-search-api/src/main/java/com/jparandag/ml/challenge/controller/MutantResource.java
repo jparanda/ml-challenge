811d6f2dd51e777c8b6a925d1c0b3c2af801d101
Juan Pablo Aranda Galvis <juan.aranda.galvis@gmail.com>
2020-06-30 11:55:08 -0500

    adding some changes

--- mutant-search-api/src/main/java/com/jparandag/ml/challenge/controller/MutantResource.java
+++ mutant-search-api/src/main/java/com/jparandag/ml/challenge/controller/MutantResource.java
@@ -4,7 +4,6 @@ import com.jparandag.ml.challenge.service.MutantService;
 import com.jparandag.ml.challenge.utils.Validation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
